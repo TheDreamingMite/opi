80e32b6779f93308ef69b671b72abac48145000f
Владимир <87864771+TheDreamingMite@users.noreply.github.com>
2026-02-10 13:52:43 +0300

    Update spidev_test.c

--- spidev_test.c
+++ spidev_test.c
@@ -118,58 +118,44 @@ static int unescape(char *_dst, char *_src, size_t len)
 
 static void transfer(int fd, uint8_t const *tx, uint8_t const *rx, size_t len)
 {
-	int ret;
-	int out_fd;
-	struct spi_ioc_transfer tr = {
-		.tx_buf = (unsigned long)tx,
-		.rx_buf = (unsigned long)rx,
-		.len = len,
-		.delay_usecs = delay,
-		.word_delay_usecs = word_delay,
-		.speed_hz = speed,
-		.bits_per_word = bits,
-	};
+    int ret;
+    struct spi_ioc_transfer tr = {
+        .tx_buf = (unsigned long)tx,
+        .rx_buf = (unsigned long)rx,
+        .len = len,
+        .delay_usecs = delay,
+        .word_delay_usecs = word_delay,
+        .speed_hz = speed,
+        .bits_per_word = bits,
+    };
 
-	if (mode & SPI_TX_OCTAL)
-		tr.tx_nbits = 8;
-	else if (mode & SPI_TX_QUAD)
-		tr.tx_nbits = 4;
-	else if (mode & SPI_TX_DUAL)
-		tr.tx_nbits = 2;
-	if (mode & SPI_RX_OCTAL)
-		tr.rx_nbits = 8;
-	else if (mode & SPI_RX_QUAD)
-		tr.rx_nbits = 4;
-	else if (mode & SPI_RX_DUAL)
-		tr.rx_nbits = 2;
-	if (!(mode & SPI_LOOP)) {
-		if (mode & (SPI_TX_OCTAL | SPI_TX_QUAD | SPI_TX_DUAL))
-			tr.rx_buf = 0;
-		else if (mode & (SPI_RX_OCTAL | SPI_RX_QUAD | SPI_RX_DUAL))
-			tr.tx_buf = 0;
-	}
+    ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+    if (ret < 1) {
+        fprintf(stderr, "ERROR: ioctl(SPI_IOC_MESSAGE) returned %d, errno=%d (%s)\n",
+                ret, errno, strerror(errno));
+        exit(1);  // вместо abort(), чтобы увидеть ошибку
+    }
 
-	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-	if (ret < 1)
-		pabort("can't send spi message");
+    if (verbose)
+        hex_dump(tx, len, 32, "TX");
 
-	if (verbose)
-		hex_dump(tx, len, 32, "TX");
+    if (output_file) {
+        int out_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        if (out_fd < 0) {
+            perror("could not open output file");
+            exit(1);
+        }
+        ret = write(out_fd, rx, len);
+        if (ret != len) {
+            perror("not all bytes written to output file");
+            close(out_fd);
+            exit(1);
+        }
+        close(out_fd);
+    }
 
-	if (output_file) {
-		out_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-		if (out_fd < 0)
-			pabort("could not open output file");
-
-		ret = write(out_fd, rx, len);
-		if (ret != len)
-			pabort("not all bytes written to output file");
-
-		close(out_fd);
-	}
-
-	if (verbose)
-		hex_dump(rx, len, 32, "RX");
+    if (verbose)
+        hex_dump(rx, len, 32, "RX");
 }
 
 static void print_usage(const char *prog)
